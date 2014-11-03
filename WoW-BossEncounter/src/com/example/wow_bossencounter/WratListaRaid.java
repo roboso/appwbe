2acafb3db334ce6233e1d927c42177206e013756
paolini diego <paolinidiego@gmail.com>
2014-11-03 14:56:17 +0100

    aggiunte immagini e provato fix alla wrat

--- WoW-BossEncounter/src/com/example/wow_bossencounter/WratListaRaid.java
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/WratListaRaid.java
@@ -1,5 +1,14 @@
 package com.example.wow_bossencounter;
 
-public class WratListaRaid {
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
+public class WratListaRaid extends ActionBarActivity  {
+	
+	
+	@Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+}
 }

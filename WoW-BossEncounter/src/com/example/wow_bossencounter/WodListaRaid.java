a45bb03353a3a3dd56c8a540cac17f8aa89dd07d
paolini diego <paolinidiego@gmail.com>
2014-11-03 22:58:35 +0100

    fatta la parte java dei raid di wod

--- WoW-BossEncounter/src/com/example/wow_bossencounter/WodListaRaid.java
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/WodListaRaid.java
@@ -1,17 +1,32 @@
 package com.example.wow_bossencounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WodListaRaid extends ActionBarActivity {
+	Button altomaglio;
+	Button fonderia;
+	
 	
 	@Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wod_list_raid);
-	
-	
+        altomaglio = (Button) this.findViewById(R.id.altomaglio);
+        fonderia = (Button) this.findViewById(R.id.fonderia);
+	}
+	public void startaltomaglio (View ImageButton) {
+		Intent intent = new Intent(this, WodRaidAltomaglio.class);
+		startActivity(intent);
+		}
+	public void startfornace (View ImageButton) {
+		Intent intent = new Intent(this, WodRaidFonderia.class);
+		startActivity(intent);
+		}
 	
 	
 	}	
-}
+

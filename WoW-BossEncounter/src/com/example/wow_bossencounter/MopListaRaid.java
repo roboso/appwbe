6ad6090940b0f878bda01cfb582706891b55f6f4
paolini diego <paolinidiego@gmail.com>
2014-11-03 23:35:07 +0100

    aggiunta la parte java di mop e la parte xml

--- WoW-BossEncounter/src/com/example/wow_bossencounter/MopListaRaid.java
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/MopListaRaid.java
@@ -1,13 +1,52 @@
 package com.example.wow_bossencounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MopListaRaid extends ActionBarActivity  {
+	
+	Button segrete;
+	Button cuore;
+	Button terrazza;
+	Button regnodeltuono;
+	Button soo;
+
+	
+	
 	
 	@Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mop_list_raid);
-}
+        segrete = (Button) this.findViewById(R.id.segrete);
+        cuore = (Button) this.findViewById(R.id.cuore);
+        terrazza = (Button) this.findViewById(R.id.terrazza);
+        regnodeltuono = (Button) this.findViewById(R.id.regnodeltuono);
+        soo = (Button) this.findViewById(R.id.soo);
+	}
+        public void listasegrete (View Button) {
+    		Intent intent = new Intent(this, MopRaidSegrete.class);
+    		startActivity(intent);
+    		}
+        public void listacuore (View Button) {
+    		Intent intent = new Intent(this, MopRaidCuore.class);
+    		startActivity(intent);
+    		}
+        public void listaterrazza (View Button) {
+    		Intent intent = new Intent(this, MopRaidAssedio.class);
+    		startActivity(intent);
+    		}
+        public void listaregnodeltuono (View Button) {
+    		Intent intent = new Intent(this, MopRaidTot.class);
+    		startActivity(intent);
+    		}
+        public void listasoo (View Button) {
+    		Intent intent = new Intent(this, MopRaidAssedio.class);
+    		startActivity(intent);
+    		}
+
 }
